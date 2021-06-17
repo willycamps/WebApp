9408875a103394190397fb90b806ea2de93c1946
willycamps <willycamps@gmail.com>
2021-06-17 17:56:24 +0200

    local changes

--- api/run.py
+++ api/run.py
@@ -1,23 +1,28 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_restful import Api,reqparse
+from flask_restful import Api
+#from app import app as application
+#from flask.ext.sqlalchemy import SQLAlchemy
+
 from views import Club
-from flask import jsonify, request
-
-
-from db import Db
-
-from sqlalchemy import text as sql_text
-
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://dbuser:dbpassword@db/users_db'
+#db = SQLAlchemy(app)
+
 CORS(app)
 api = Api(app)
-
+##
+## Actually setup the Api resource routing here
+##
 
 api.add_resource(Club, '/clubs',methods=['POST', 'GET'])
 
 
-if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0')
+@app.route('/')
+def index():
+    return '<h1> Hello World ! </h1>'
 
+if __name__ == '__main__':
+    #db.create_all()
+    app.run(debug = True, host = '0.0.0.0')
